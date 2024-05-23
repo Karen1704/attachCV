b597759650cfe94e1ff909fc06d8458d9812a4e2
Karen1704 <kar.ghalahyan@gmail.com>
2024-05-23 16:59:49 +0400

    change recepient

--- routes/email.js
+++ routes/email.js
@@ -46,7 +46,7 @@ emailRouter.post('/contactForm', upload.single('attachment'), async (req, res) =
         const { firstName, lastName, phone, email, message } = req.body;
         // Email data
         const msg = {
-            to: "avetyanvaleri@gmail.com",
+            to: process.env.RECEPIENT_EMAIL,
             from: process.env.SENDER_EMAIL, 
             subject: `${firstName} ${lastName}`,
             html:`<p><strong>First Name:</strong>${firstName}</p> <p><strong>Last Name:</strong>${lastName}</p>  <p><strong>Email:</strong>${email}</p> <p><strong>Phone:</strong>${phone}</p>   <p><strong>Message:</strong>${message}</p>`,
